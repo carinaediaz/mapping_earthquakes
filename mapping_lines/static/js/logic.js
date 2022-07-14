4d4f11bf06ae8a772bd4f165d988bed41dd8e770
carinaediaz <carinaediaz96@gmail.com>
2022-07-13 19:46:39 -0500

    using polyline to plot flight routes

--- mapping_lines/static/js/logic.js
+++ mapping_lines/static/js/logic.js
@@ -1,22 +1,27 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([36.1733,-120.1794], 7)
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([40.7, -94.5], 5);
 
-// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
 let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790]
+  [37.6213, -122.3790],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
 ];
 
-// Create a polyline using the line coordinates and make the line red.
+// Create a polyline using the line coordinates and make the line yellow.
 L.polyline(line, {
-  color: "red"
+  color: "blue",
+  weight: 4,
+  opacity: 0.5,
+  dashArray: 8
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
